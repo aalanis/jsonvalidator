1b312e3b5ad9b5a649117276a6d736931206dc98
Alanis Gonzalez, Jose Adrian <ze13ju@gmail.com>
2025-09-05 12:03:36 +0200

    feat(validator): split generation and validation of schema

--- jsonvalidator/validator.py
+++ jsonvalidator/validator.py
@@ -1,28 +1,33 @@
-from os import wait
 import yaml
 from loguru import logger
 from pathlib import Path
 from genson import SchemaBuilder
 from jsonschema import validate
+
 import json
+
+
+def generate_schema():
+    with open(f"{Path.cwd()}/pipe_minimal.yml") as f:
+        json_schema = yaml.full_load(f)
+    builder = SchemaBuilder(schema_uri="https://json-schema.org/draft/2020-12/schema")
+    builder.add_object(json_schema)
+
+    schema = builder.to_json(indent=2)
+
+    with open(f"{Path.cwd()}/schema.json", "w") as f:
+        f.write(schema)
 
 
 def run():
     with open(f"{Path.cwd()}/pipe_minimal.yml") as f:
         val = yaml.full_load(f)
-        logger.info(f"{type(val)}")
-        logger.info(f"{val}")
 
-        # builder = SchemaBuilder()
-        # builder.add_object(val)
-        #
-        # schema = builder.to_json(indent=2)
-        # logger.debug(f"{schema}")
+    json_schema = json.load(open(f"{Path.cwd()}/schema.json"))
 
-        # with open(f"{Path.cwd()}/schema.json", "w") as f:
-        # f.write(schema)
+    try:
+        validate(val, schema=json_schema)
+        logger.info("schema is valid")
 
-        schema = json.load(open(f"{Path.cwd()}/schema.json"))
-
-        validate(val, schema=schema)
-        :wait()
+    except Exception as e:
+        raise e
